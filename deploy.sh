cb26a0d21eb87ecf7fa1efff3559d780a183cfc3
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-12-21 12:05:35 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:Dcode-Studios/slashroles-website.git main:gh-pages
 
 cd -
